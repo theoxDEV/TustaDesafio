d216579e61412c905b0c85ff8b2328176c6e34e6
Matheus Nascimento <56891192+theoxDEV@users.noreply.github.com>
2022-09-01 16:30:13 -0300

    Adicionar arquivos de projeto.

---
+++ TustaDesafio/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TustaDesafio
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // declarações de variáveis
+            float valorMonetario;
+            int numeroParcelas;
+
+            // primeiro input do valor monetário
+            Console.Write("Digite um valor em R$ ");
+            valorMonetario = float.Parse(Console.ReadLine());
+
+            // segundo input contendo o número de parcelas
+            Console.Write("Digite o número de parcelas: ");
+            numeroParcelas = int.Parse(Console.ReadLine());
+
+            CalculaParcelas(valorMonetario, numeroParcelas);
+        }
+
+        public static float CalculaParcelas(float valorMonetario, int numeroParcelas)
+        {
+            // valor de cada parcela
+            float valorParcelas = valorMonetario / numeroParcelas;
+
+            // loop para apresentar os valores das parcelas
+            for (int parcela = 1; parcela <= numeroParcelas; parcela++)
+            {
+                Console.WriteLine($"A {parcela}º parcela é de {valorParcelas:C}");
+            }
+
+            return valorParcelas;
+        }
+    }
+}
---
+++ TustaDesafioTests/ProgramTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TustaDesafio.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void Program_shouldReturnNotEqual()
+        {
+            string VALOR_MONETARIO_MOCK = "555,00";
+            int NUMERO_PARCELAS_MOCK = 12;
+            float VALOR_PARCELA_MOCK = 50;
+
+            Assert.AreNotEqual(VALOR_PARCELA_MOCK, Program.CalculaParcelas(float.Parse(VALOR_MONETARIO_MOCK), NUMERO_PARCELAS_MOCK));
+        }
+
+        [TestMethod()]
+        public void Program_shouldReturnEqual()
+        {
+            string VALOR_MONETARIO_MOCK = "720,00";
+            int NUMERO_PARCELAS_MOCK = 12;
+            float VALOR_PARCELA_MOCK = 60;
+
+            Assert.AreEqual(VALOR_PARCELA_MOCK, Program.CalculaParcelas(float.Parse(VALOR_MONETARIO_MOCK), NUMERO_PARCELAS_MOCK));
+        }
+    }
+}
